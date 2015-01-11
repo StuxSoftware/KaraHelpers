369eddfadbea94de982ef7cd2f1b52ee49271be3
StuxSoftware <netzmit@gmail.com>
2015-01-11 15:44:28 +0100

    Allow values to be recalled.

--- Utilities.lua
+++ Utilities.lua
@@ -5,4 +5,4 @@ function toarray(...) local result = {}; local i = 1; for item in ... do result[
 function loadEnvironment() for key, value in pairs(tenv._G) do if tenv[key] == nil then tenv[key] = value; end; end; end;
 
 -- Sets a value
-function setValue(name, value, obj) if obj==nil then obj = tenv; end; obj[name] = value; return ""; end;
+function setValue(name, value, obj, recallable) if recallable==nil then recallable=false end; if obj==nil then obj = tenv; if recallable then tenv.remember(name, value); end; end; obj[name] = value; end; return ""; end;
